1d96bf55e9494347018f6c5ca9c790a3742bdcdb
Ryota Kameoka <kameoka.ryota@gmail.com>
2018-04-14 23:45:13 +0900

    Replace peco with fzf

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -1,7 +1,20 @@
+FPATH=$ZDOTDIR/functions:$FPATH
+
 autoload -Uz clean-up-swap-files
 
-autoload -Uz peco-history-search
-autoload -Uz peco-select-gitadd
+load() {
+  autoload -Uz $1
+  zle -N $1
+  if [ "$2" != '' ]; then
+    bindkey $2 $1
+  fi
+}
 
-zle -N peco-select-gitadd
-bindkey "^g^a" peco-select-gitadd
+if which fzf > /dev/null; then
+  load fzf-history-search '^R'
+
+  load fzf-git-add '^G^A'
+
+  load fzf-git-checkout-branch
+  alias gco=fzf-git-checkout-branch
+fi
--- .zsh/history.zsh
+++ .zsh/history.zsh
@@ -10,11 +10,3 @@ zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
 bindkey "^[p" history-beginning-search-backward-end
 bindkey "^[n" history-beginning-search-forward-end
-
-FPATH=$ZDOTDIR/functions:$FPATH
-autoload -Uz peco-history-search
-zle -N peco-history-search
-
-if which peco > /dev/null; then
-  bindkey '^R' peco-history-search
-fi
