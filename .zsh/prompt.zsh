496499a24b2da996aa48b03f739120de8db47cfe
Ryota Kameoka <kameoka.ryota@gmail.com>
2018-04-14 21:45:27 +0900

    Simplify prompt declaration

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -2,9 +2,6 @@ autoload -Uz vcs_info
 zstyle ':vcs_info:*' formats '(%s)-[%b]'
 zstyle ':vcs_info:*' actionformats '(%s)-[%b|%a]'
 
-local pr_cd="%F{cyan}[%~]%f     %1(v|%F{green}%1v%f|)";
-local pr_info="%(?.%F{yellow}%}.%F{magenta})%n@%m%f"
 PROMPT="
-$pr_cd
-$pr_info $ ";
-RPROMPT=""
+%F{cyan}[%~]%f %1(v|%F{green}%1v%f|)
+%(?.%F{yellow}%}.%F{magenta})%n@%m%f $ "
