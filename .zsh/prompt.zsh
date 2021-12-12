e474cb95480be488a2005bcfbbde238f9d7252c6
Ryota Kameoka <kameoka.ryota@gmail.com>
2021-12-12 18:30:55 +0900

    Simplify prompt

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,6 +1,6 @@
 autoload -Uz vcs_info
-zstyle ':vcs_info:*' formats '(%s)-[%b]'
-zstyle ':vcs_info:*' actionformats '(%s)-[%b|%a]'
+zstyle ':vcs_info:*' formats '%s: %b'
+zstyle ':vcs_info:*' actionformats '%s: %b (%a)'
 
 PROMPT="
 %F{blue}[%~]%f %1(v|%F{green}%1v%f|)
